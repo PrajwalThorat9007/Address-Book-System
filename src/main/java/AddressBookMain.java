66c20f1d678aab49ea4eec074d1677ec34de4c9d
Prajwal Sanjay Thorat <prajwalthorat9007@gmail.com>
2026-03-02 23:48:25 +0530

    [Prajwal]:UC6: Refactored system to support multiple AddressBooks using Map with unique name

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AddressBookMain {
@@ -6,19 +8,68 @@ public class AddressBookMain {
 
         System.out.println("Welcome to Address Book Program");
 
-        AddressBook addressBook = new AddressBook();
+        Map<String, AddressBook> addressBookMap = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
 
-        boolean exit = false;
+        boolean exitSystem = false;
 
-        while (!exit) {
+        while (!exitSystem) {
 
-            System.out.println("\nChoose an Option:");
+            System.out.println("\nMain Menu:");
+            System.out.println("1. Create New Address Book");
+            System.out.println("2. Open Existing Address Book");
+            System.out.println("3. Exit");
+
+            int choice = Integer.parseInt(scanner.nextLine());
+
+            switch (choice) {
+
+                case 1:
+                    System.out.println("Enter Address Book Name:");
+                    String bookName = scanner.nextLine();
+
+                    if (addressBookMap.containsKey(bookName)) {
+                        System.out.println("Address Book already exists!");
+                    } else {
+                        addressBookMap.put(bookName, new AddressBook());
+                        System.out.println("Address Book Created Successfully!");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Enter Address Book Name:");
+                    String existingBook = scanner.nextLine();
+
+                    if (addressBookMap.containsKey(existingBook)) {
+                        manageAddressBook(addressBookMap.get(existingBook), scanner);
+                    } else {
+                        System.out.println("Address Book Not Found!");
+                    }
+                    break;
+
+                case 3:
+                    exitSystem = true;
+                    System.out.println("Exiting System...");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice!");
+            }
+        }
+    }
+
+    private static void manageAddressBook(AddressBook addressBook, Scanner scanner) {
+
+        boolean exitBook = false;
+
+        while (!exitBook) {
+
+            System.out.println("\nAddress Book Menu:");
             System.out.println("1. Add Contact");
             System.out.println("2. Edit Contact");
             System.out.println("3. Delete Contact");
-            System.out.println("4. Display All Contacts");
-            System.out.println("5. Exit");
+            System.out.println("4. Display Contacts");
+            System.out.println("5. Back to Main Menu");
 
             int choice = Integer.parseInt(scanner.nextLine());
 
@@ -43,8 +94,7 @@ public class AddressBookMain {
                     break;
 
                 case 5:
-                    exit = true;
-                    System.out.println("Exiting Address Book...");
+                    exitBook = true;
                     break;
 
                 default:
