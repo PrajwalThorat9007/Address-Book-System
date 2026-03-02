be634464f64a31db053f053e70c049d99fc364e1
Prajwal Sanjay Thorat <prajwalthorat9007@gmail.com>
2026-03-02 23:29:01 +0530

    [Prajwal]:UC2: Added AddressBook class and console-based contact creation using OOP relationship

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -4,18 +4,7 @@ public class AddressBookMain {
 
         System.out.println("Welcome to Address Book Program");
 
-        // Sample Contact Object (for UC1 validation)
-        Contact contact = new Contact(
-                "Prajwal",
-                "Thorat",
-                "MG Road",
-                "Chennai",
-                "Tamil Nadu",
-                "600001",
-                "9876543210",
-                "prajwal@email.com"
-        );
-
-        System.out.println(contact);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
     }
 }
